5df3c8b3a3d37a712531072c8f2ef9673bf7ee26
Eric Newton <ericnewton76@gmail.com>
2013-02-04 00:44:01 -0500

    modified version and removed strong name key

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("HttpCookieEncryption")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ensoft Software, Inc.")]
+[assembly: AssemblyCompany("Erinew Soft, Inc.")]
 [assembly: AssemblyProduct("HttpCookieEncryption")]
-[assembly: AssemblyCopyright("Copyright(C) 2004, Ensoft Software, Inc.")]
+[assembly: AssemblyCopyright("Copyright(C) 2012, Erinew Soft, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.100")]
+[assembly: AssemblyVersion("1.0.0.200")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -55,4 +55,4 @@ using System.Runtime.CompilerServices;
 //
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
-[assembly: AssemblyKeyName("Ensoft")]
+//[assembly: AssemblyKeyName("Ensoft")]
