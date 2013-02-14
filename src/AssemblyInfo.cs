0e07d35456cf36ceb31a9004659a58accb2ab289
Eric Newton <ericnewton76@gmail.com>
2013-02-13 22:36:53 -0500

    bumped version to 0.1.1

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyFileVersionAttribute("0.1.0.0")]
 [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.1.15813")]
