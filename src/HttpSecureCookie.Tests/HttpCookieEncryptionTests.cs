7b7c8339ab5aa5dedabe09f7263ab0122564365d
Eric Newton <ericnewton76@gmail.com>
2013-02-13 21:35:48 -0500

    added tests to the testing project

---
+++ src/HttpSecureCookie.Tests/HttpCookieEncryptionTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using System.Web;
+
+namespace HttpSecureCookie.Tests
+{
+	[TestFixture]
+	class HttpCookieEncryptionTests
+	{
+		[Test]
+		public void static_ctor_worked()
+		{
+			//just have to call something on the type itself.
+			System.Web.HttpCookieEncryption.Equals(new object(), new object());
+		}
+
+		[Test]
+		public void Encrypt_Cookie()
+		{
+			string startingValue = "asdf";
+
+			HttpCookie cookie = new HttpCookie("test");
+			cookie.Value = startingValue;
+
+			cookie = HttpCookieEncryption.Encrypt(cookie);
+			string actualValue = cookie.Value;
+
+			Assert.AreNotEqual(startingValue, actualValue);
+		}
+
+		[Test]
+		public void Encrypt_Decrypt_roundtrip()
+		{
+			string startingValue = "asdf";
+
+			HttpCookie cookie = new HttpCookie("test");
+			cookie.Value = startingValue;
+
+			HttpCookie encrypted = HttpCookieEncryption.Encrypt(cookie);
+
+			HttpCookie decrypted = HttpCookieEncryption.Decrypt(encrypted);
+
+			Assert.AreEqual(decrypted.Value, cookie.Value);
+		}
+
+	}
+}
---
+++ src/HttpSecureCookie.Tests/MachineKeyWrapperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace HttpSecureCookie.Tests
+{
+	[TestFixture]
+	class MachineKeyWrapperTests
+	{
+		[Test]
+		public void static_ctor_worked()
+		{
+			//just have to call something on the type itself.
+			System.Web.Security.MachineKeyWrapper.ReferenceEquals(new object(), new object());
+		}
+
+		[Test]
+		public void EncryptOrDecrypt()
+		{
+			byte[] data = System.Text.Encoding.UTF8.GetBytes("This is a test string.");
+			byte[] mod = new byte[] { };
+			System.Web.Security.MachineKeyWrapper.EncryptOrDecryptData(true, data, mod, 0, data.Length);
+		}
+
+	}
+}
