a5b255e0dc36a877f2b569c7ad07a3251fb9b45a
Keyur <keyurpd@users.noreply.github.com>
2016-03-30 19:03:36 +0530

    Made version 0.2.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.1.15813")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.1.15813")]
